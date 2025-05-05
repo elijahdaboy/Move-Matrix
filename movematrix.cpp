45ae15e02551766d0b4aac44f1741ff330461793
Elijah Aaron <elijahhager20@gmail.com>
2025-05-05 13:09:58 -0600

    Update movematrix.cpp

--- movematrix.cpp
+++ movematrix.cpp
@@ -19,8 +19,8 @@ class MoveVector{
         }
     
         std::vector<std::vector<int>> moveMatrixDownFunc(std::vector<std::vector<int>> vectorMain, int vectorMainHeight, int vectorMainLength){
-            for (int i = 0; i < vectorMainHeight - 1; i++){
-                for (int j = 0; j < vectorMainLength; j++){
+            for (int i = vectorMainHeight - 1; i > 0; i--){
+                for (int j = vectorMainLength; j > 0; j--){
                     if (i != 0){
                         std::swap(vectorMain[i][j], vectorMain[i + 1][j]);
                     } else{
